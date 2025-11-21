03ac329012708165830db24cbd6aa23431eb74a9
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-21 21:42:46 +0530

    Exercise 2 complete

--- 01. Basic/app.js
+++ 01. Basic/app.js
@@ -1,7 +1,20 @@
 // Exercise 1
-
+/*
 let name = "Raja Mahanty";
 let whatDoYouWannaBecomeInYourLife = "Happy";
 let gender = "Male";
 let twitterHandle = "@RajaMahanty7252";
 console.table({ name, whatDoYouWannaBecomeInYourLife, gender, twitterHandle });
+*/
+
+// Exercise 2
+
+let firstFavNum = 56;
+let secondFavNum = 38;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNum - secondFavNum);
+console.log(firstFavNum * secondFavNum);
+console.log(firstFavNum / secondFavNum);
+console.log(firstFavNum % secondFavNum);
+console.log(firstFavNum ** secondFavNum);
