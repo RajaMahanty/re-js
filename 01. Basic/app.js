a98a779ac22949ea7aaa176d6c99d86c8933693c
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-21 21:34:41 +0530

    Exercise 1 complete

---
+++ 01. Basic/app.js
@@ -0,0 +1,7 @@
+// Exercise 1
+
+let name = "Raja Mahanty";
+let whatDoYouWannaBecomeInYourLife = "Happy";
+let gender = "Male";
+let twitterHandle = "@RajaMahanty7252";
+console.table({ name, whatDoYouWannaBecomeInYourLife, gender, twitterHandle });
