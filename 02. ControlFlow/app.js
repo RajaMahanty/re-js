713e44b5f0463efeaa3460df70164aebd5371f9f
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-23 17:06:39 +0530

    Exercise 6 complete

---
+++ 02. ControlFlow/app.js
@@ -0,0 +1,11 @@
+// Exercise 6
+let number = 8;
+if (number === 8) {
+	console.log("Welcome");
+} else if (number <= 8) {
+	console.log("number too short");
+} else if (number >= 8) {
+	console.log("number too long\nMust be 8 characters long");
+} else {
+	console.log("Please provide a number");
+}
