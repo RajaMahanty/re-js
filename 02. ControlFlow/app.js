daab32a085b90359afabf4441a45a13416f6cb38
Raja Mahanty <rajamahanty2@gmail.com>
2025-11-23 19:32:48 +0530

    Exercise 7 complete

--- 02. ControlFlow/app.js
+++ 02. ControlFlow/app.js
@@ -1,4 +1,22 @@
+// Exercise 7
+
+let fruit = "Banana";
+switch (fruit) {
+	case "Banana":
+		console.log("Banana is good");
+		break;
+	case "Orange":
+		console.log("I am a fan of orange");
+		break;
+	case "Apple":
+		console.log("Apple is ok");
+		break;
+	default:
+		console.log("Yeah what's that?");
+}
+
 // Exercise 6
+/*
 let number = 8;
 if (number === 8) {
 	console.log("Welcome");
@@ -9,3 +27,4 @@ if (number === 8) {
 } else {
 	console.log("Please provide a number");
 }
+*/
