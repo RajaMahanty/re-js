2c117000aa28d6eb9d0cb779198546221ac30cce
Raja Mahanty <rajamahanty2@gmail.com>
2026-01-18 13:13:25 +0530

    Add exercises 10, 11, and 12 to app.js

---
+++ 03. Arrays Objects/app.js
@@ -0,0 +1,38 @@
+// {
+// 	var text1 = "Hello";
+// }
+// console.log(text1);
+
+// Exercise 12
+
+// function showCallFunc(fn) {
+// 	const value = 10;
+// 	fn(value);
+// }
+
+// showCallFunc(function (value) {
+// 	console.log(value);
+// });
+
+// Exercise 11
+
+// function myFunction(num1, num2) {
+// 	return num1 * num2;
+// }
+
+// let result = myFunction(4, 5);
+// console.log(result);
+
+// Exercise 10
+
+// const car = {
+//   type: "Four Wheeler",
+//   model: "SUV",
+//   color: "Red",
+// }
+
+// console.log(typeof(car));
+
+// car.type = "Toyota";
+
+// console.log(car.type);
